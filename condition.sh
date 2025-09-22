d4bfe946c1fee09b02bac73e40d953f4c4732d23
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-22 15:34:15 -0500

    sample1

---
+++ condition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+number=$1
+
+if [$number -lt 20]; then 
+    echo "number is less than 20"
+elif [$number -eq 20]; then 
+    echo "number is equal to 20"
+else 
+    echo "number is greater than 20"
+fi 
