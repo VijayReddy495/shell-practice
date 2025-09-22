dc6089b360b9f31a758bfc608d64b0b203d8b9ac
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-22 17:04:34 -0500

    sample1

--- func.sh
+++ func.sh
@@ -2,6 +2,12 @@
 
 userid=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+
 if [ $userid -ne 0 ]; then
     echo "user don't have previliage access to install "
     exit 1
@@ -9,19 +15,35 @@ fi
 
 validate(){
    if [ $1 -eq 0 ]; then 
-    echo "$2 has been successfully installed"
+    echo -e "$2 has been ...$G successfully $N installed"
    else
-    echo "there is error installing $2 package"
+    echo -e "there is...$R error $N installing $2 package"
     exit 1
    fi
 }
 
-dnf install mysql -y
-validate $? "mysql"
+dnf list installed mysql
+if [ $? -ne 0]; then 
+    dnf install mysql -y
+    validate $? "mysql"
+else    
+    echo -e "mysql is already ...$G installed $N"
+fi     
 
-dnf install nginx -y
-validate $? "nginx"
+dnf list installed nginx 
+if [ $? -ne 0]; then 
+    dnf install nginx -y
+    validate $? "nginx"
+else 
+    echo -e "nginx is already ...$G installed $N"
+fi    
 
-dnf install python3 -y
-validate $? "python3"
+dnf list installed python3
+if [ $? -ne 0]; then 
+    dnf install python3 -y
+    validate $? "python3"
+else 
+    echo -e "python3 is already ...$G installed $N"
+fi        
+
 
