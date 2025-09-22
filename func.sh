21ebfb4f96f24e489b3c27f9abd2d196836619e5
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-22 17:47:09 -0500

    sample1

--- func.sh
+++ func.sh
@@ -2,6 +2,7 @@
 
 userid=$(id -u)
 
+
 R="\e[31m"
 G="\e[32m"
 y="\e[33m"
@@ -11,39 +12,45 @@ N="\e[0m"
 if [ $userid -ne 0 ]; then
     echo "user don't have previliage access to install "
     exit 1
-fi  
+fi 
+
+Log_Folder="/var/log/shell-script"
+Script_Name=$(echo $0 | cut -d "." -f1) 
+Log_File="$Log_Folder/$Script_Name.log"
+
+mkdir -p Log_Folder
 
 validate(){
    if [ $1 -eq 0 ]; then 
-    echo -e "$2 has been ...$G successfully $N installed"
+    echo -e "$2 has been ...$G successfully $N installed" | tee -a Log_File
    else
-    echo -e "there is...$R error $N installing $2 package"
+    echo -e "there is...$R error $N installing $2 package" | tee -a Log_File
     exit 1
    fi
 }
 
-dnf list installed mysql
+dnf list installed mysql >> Log_File
 if [ $? -ne 0 ]; then 
     dnf install mysql -y
     validate $? "mysql"
 else    
-    echo -e "mysql is already ...$G installed $N"
+    echo -e "mysql is already installed, So ...$Y Skipping $N" | tee -a Log_File 
 fi     
 
-dnf list installed nginx 
+dnf list installed nginx Log_File
 if [ $? -ne 0 ]; then 
     dnf install nginx -y
     validate $? "nginx"
 else 
-    echo -e "nginx is already ...$G installed $N"
+    echo -e "nginx is already installed, So ...$Y Skipping $N" | tee -a Log_File
 fi    
 
-dnf list installed python3
+dnf list installed python3 Log_File
 if [ $? -ne 0 ]; then 
     dnf install python3 -y
     validate $? "python3"
 else 
-    echo -e "python3 is already ...$G installed $N"
+    echo -e "python3 is already installed, So ...$Y Skipping $N" | tee -a Log_File
 fi        
 
 
