c5cc11b33412879dd6bd5bbcbbee6fcfda2a5572
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-22 14:54:58 -0500

    sample1

--- specialvaribales.sh
+++ specialvaribales.sh
@@ -3,5 +3,13 @@
 echo "printing all the variaables : $@"
 echo "current running script : $0"
 echo "PID number of the current script :$$"
-sleep 25
+sleep 25 &
 echo "PID of the last command in sleep :$!"
+
+number1=110
+number2=220
+string1=devops
+
+addition_result = sum((number1+number2+string1))
+
+echo "sum of the numbers is :$addition_result"
