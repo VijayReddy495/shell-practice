1f6a4c0803e6a84a03fcaf5bc5ec7078ed9f2f55
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-22 14:34:55 -0500

    sample

---
+++ specialvaribales.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "printing all the variaables : $@"
+echo "current running script : $0"
+echo "PID number of the current script :$$"
+sleep 25
+echo "PID of the last command in sleep :$!"
