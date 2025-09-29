b85b530e75134490104535a9c8d8c8d4d2ba0ebc
vijay reddy <vijaybhaskerreddy04@gmail.com>
2025-09-29 17:08:32 -0500

    test

--- logdeletion.sh
+++ logdeletion.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 Log_Dir="/var/log/shell-practice"
-Log_FileName=$( echo $0 | cut -d "." f1)
+Log_FileName=$( echo $0 | cut -d "." -f1)
 Log_Path="$Log_Dir/$Log_FileName.log"
 
 mkdir -p $Log_Path
 echo "script started executing at :$(date)" | tee -a $Log_Path
 
-Source_Dir="/home/ec2-user/app-logs"
+Source_Dir=/home/ec2-user/app-logs
 
 if[ ! -d $Source_Dir ]; then 
     echo "Source Directory doesn't exists"
